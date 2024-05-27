2c57ee529c20bb0dc37404337e32245fceee4afa
James Barnes <jamesbarnes20082000@gmail.com>
2024-05-27 14:24:07 +0100

    create dynamic navbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,11 +1,34 @@
+<script>
+export default {
+    name: 'NavigationBar',
+    data: () => ({
+        drawer: null,
+        items: [
+            {name: 'Home', path: '/', icon: 'mdi-home'},
+            {name: 'Showcase', path: '/showcase', icon: 'mdi-movie-filter'}
+        ]
+    }),
+
+}
+</script>
+
 <template>
-    <v-app-bar id="navbar" scroll-behavior="hide" collapse rounded>
-        <v-app-bar-nav-icon />
-        <v-navigation-drawer>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/showcase">Showcase</RouterLink>
-        </v-navigation-drawer>
+    <v-app-bar id="navbar" collapse rounded>
+        <v-app-bar-nav-icon @click="drawer = !drawer" />
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+        <v-list dense nav>
+            <v-list-item
+                v-for="item in items"
+                :key="item.name"
+                :prepend-icon="item.icon"
+                :title="item.name"
+                dense
+                router :to="item.path"
+            >
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
 </template>
 
 <style scoped>
