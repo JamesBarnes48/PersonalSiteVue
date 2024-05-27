a0853b1abcd3bb0904f7f383de50648c47e6cfa9
James Barnes <jamesbarnes20082000@gmail.com>
2024-05-27 16:38:26 +0100

    create basic SkillsPanel component

---
+++ src/components/SkillsPanel.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'SkillPanel',
+    props: {
+        skillName: {type: String, default: 'A Skill'},
+        description: {type: 'String', default: 'Skill Description'},
+        listDescription: {type: 'String'},
+        listItems: {type: 'Object', default() {return []}}
+    }
+}
+</script>
+
+<template>
+    <v-expansion-panel
+    :title="skillName"
+    >
+        <v-expansion-panel-text>
+            <p>{{ description }}</p>
+            <div v-if="listItems.length">
+                <p>{{ listDescription }}</p>
+                <v-list>
+                    <v-list-item 
+                    v-for="item in listItems"
+                    :key="item"
+                    :title="item" />
+                </v-list>
+            </div>
+        </v-expansion-panel-text>
+    </v-expansion-panel>
+</template>
+
+<style scoped>
+</style>
