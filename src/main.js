fb56c89325c53a1c64ea3340d464255952cef0a4
James Barnes <jamesbarnes20082000@gmail.com>
2024-05-29 14:19:57 +0100

    reconfig fontawesome

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import FontAwesomeIcon from "./font-awesome-icons.js";
+
 const vuetify = createVuetify({
     components,
     directives,
@@ -21,4 +23,5 @@ const app = createApp(App);
 
 app.use(router)
 app.use(vuetify)
+app.component("FontAwesomeIcon", FontAwesomeIcon)
 app.mount('#app')
