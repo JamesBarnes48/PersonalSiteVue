ef19b0c6b66a8c3571a5025a2ba66e6975b6e733
James Barnes <jamesbarnes20082000@gmail.com>
2024-05-30 14:40:01 +0100

    initialise showcaseproject component

---
+++ src/components/ShowcaseProject.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'ShowcaseProject'
+    props: {
+        title: {type: String, default: 'Project title'},
+        mainText: {type: String, default: 'Main text'},
+        repoLink: {type: String, default: '<link to repo>'},
+        projectImages: {type: Array}
+    }
+}
+</script>
+
+<template>
+    <div class="project-container">
+        <v-carousel>
+            <v-carousel-item
+            v-for="img in projectImages"
+            :src="img"
+            cover
+            ></v-carousel-item>
+        </v-carousel>
+        <div class="text-container">
+            <h3 class="subheading">{{ title }}</h3>
+        <p>{{ mainText }}</p>
+        <p>Try cloning the repo and follow the .readme's instructions to set it up yourself <a href="https://github.com/JamesBarnes48/Virtual-Garage">here</a></p>
+        </div>
+    </div>
+</template>
+
+<style>
+</style>
