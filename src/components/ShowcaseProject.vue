f9957d6253d141b4fa7f31b54f81617c2f6c8711
James Barnes <jamesbarnes20082000@gmail.com>
2024-05-31 12:59:10 +0100

    carousel styling

--- src/components/ShowcaseProject.vue
+++ src/components/ShowcaseProject.vue
@@ -4,6 +4,7 @@ export default {
     props: {
         title: {type: String, default: 'Project title'},
         mainText: {type: String, default: 'Main text'},
+        repoLinkText: {type: String, default: 'Find the Github repository here: '},
         repoLink: {type: String, default: '<link to repo>'},
         projectImages: {type: Array, default() {return []}}
     }
@@ -12,22 +13,29 @@ export default {
 
 <template>
     <div class="project-container">
-        <v-carousel>
-            <v-carousel-item
-            v-for="img in projectImages"
-            :src="img"
-            ></v-carousel-item>
-        </v-carousel>
+        <div class="carousel-container">
+            <v-carousel>
+                <v-carousel-item
+                v-for="img in projectImages"
+                :src="img"
+                ></v-carousel-item>
+            </v-carousel>
+        </div>
         <div class="text-container">
             <h3 class="subheading">{{ title }}</h3>
         <p>{{ mainText }}</p>
-        <p>Try cloning the repo and follow the .readme's instructions to set it up yourself <a href="https://github.com/JamesBarnes48/Virtual-Garage">here</a></p>
+        <p>{{ repoLinkText }} <a href="https://github.com/JamesBarnes48/Virtual-Garage">here</a></p>
         </div>
     </div>
 </template>
 
-<style>
+<style scoped>
 .project-container{
-    margin: 60px 50px;
+    margin: 80px 65px;
+}
+
+.carousel-container {
+    width: 60%;
+    margin: auto;
 }
 </style>
