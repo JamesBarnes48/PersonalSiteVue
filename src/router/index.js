dc95284fbc6e02f05a8f0fafce32963e71b0b70e
James Barnes <jamesbarnes20082000@gmail.com>
2024-06-08 01:33:06 +0100

    restore scroll pos to top on page chng

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,9 @@ import PageNotFound from '../views/PageNotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    return {top: 0};
+  },
   routes: [
     {
       path: '/',
