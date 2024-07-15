bd480a4628ba23eb80b023967f3f708b9b96be36
James Barnes <jamesbarnes20082000@gmail.com>
2024-07-15 13:29:44 +0100

    import icons

--- src/utilities/font-awesome-icons.js
+++ src/utilities/font-awesome-icons.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-library.add(faGithub, faLinkedin);
+import { faGithub, faLinkedin, faVuejs, faJava, faNode } from "@fortawesome/free-brands-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+library.add(faGithub, faLinkedin, faVuejs, faJava, faNode, faGlobe);
 
 export default FontAwesomeIcon;
