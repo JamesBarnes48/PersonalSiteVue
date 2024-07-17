d523012c1b318c0f0d8ce01f3bcfbaaf1b750273
James Barnes <jamesbarnes20082000@gmail.com>
2024-07-17 16:21:58 +0100

    style erdtree bg

--- src/views/Armoury.vue
+++ src/views/Armoury.vue
@@ -8,11 +8,7 @@ export default {
   },
   data() {
     return {
-      erdtreeImgProps: {
-        'backgroundImage': `url(${erdtreeImage})`,
-        'background-size': 'contain',
-        'background-position': 'center'
-      }
+      erdtreeImage: erdtreeImage,
     }
   },
   props: {
@@ -27,7 +23,8 @@ export default {
 <template>
   <div class="armoury-container">
     <h1 class="armoury-title">Armoury</h1>
-    <div class="build-container" :style="erdtreeImgProps">
+    <div class="build-container">
+      <img :src="erdtreeImage" class="erdtree-image">
 
     </div>
   </div>
@@ -48,6 +45,27 @@ export default {
   border: 1px solid var(--main-hex);
   background-color: #24211a;
   height: 650px;
+}
+
+.erdtree-image {
+  position: absolute; 
+  left: 0; 
+  right: 0; 
+  bottom: 0;
+  top: 0;
+  margin: auto;
+  object-fit: cover;
+  width: 35%;
+  opacity: 0.5;
+  filter: alpha(opacity=50);  
+  -moz-opacity: 0.5;  
+  -khtml-opacity: 0.5; 
+}
+
+@media(max-width: 1280px){
+  .erdtree-image {
+    width: 43%;
+  }
 }
 
 </style>
