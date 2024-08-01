79090014f8d8e2ea29c4b9022c2a9d1f82e78fb3
James Barnes <jamesbarnes20082000@gmail.com>
2024-08-01 16:28:41 +0100

    add deaths poker

--- src/assets/data/armaments.js
+++ src/assets/data/armaments.js
@@ -1,7 +1,12 @@
 export default [
     {
-        name: 'Bloody Helice',
-        apiRoute: 'https://eldenring.fanapis.com/api/weapons?name=Bloody%20Helice',
-        description: 'great sword'
-      }
+      name: 'Bloody Helice',
+      apiRoute: 'https://eldenring.fanapis.com/api/weapons?name=Bloody%20Helice',
+      description: 'great sword'
+    },
+    {
+      name: 'Death\'s Poker',
+      apiRoute: 'https://eldenring.fanapis.com/api/weapons?name=Death%27s%20Poker',
+      description: 'great poker'
+    }
 ]
