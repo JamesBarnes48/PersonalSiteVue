a7fd873ff9577fb1a832492961c7e6671b0dcf43
James Barnes <jamesbarnes20082000@gmail.com>
2024-09-05 23:47:20 +0100

    make for bigger screens

--- src/components/RankingBar.vue
+++ src/components/RankingBar.vue
@@ -89,6 +89,12 @@ export default {
 }
 
 @media(max-width: 700px){
+    .bar-container{
+        padding: 3px 4px;
+        margin-left: 5px;
+        margin-right: 5px;
+    }
+    
     .item-image{
         height: 75px;
     }
@@ -103,11 +109,6 @@ export default {
 }
 
 @media(max-width: 600px){
-    .bar-container{
-        padding: 3px 4px;
-        margin-left: 5px;
-        margin-right: 5px;
-    }
 
     .item-image{
         height: 51px;
